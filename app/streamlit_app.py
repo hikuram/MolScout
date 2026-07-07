14a7a4ad8250eb255fac3e2ba62a4a414cf8444d
hikuram <88078236+hikuram@users.noreply.github.com>
2026-07-07 12:26:04 +0900

    Add Chemiscope page to Streamlit app

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -28,6 +28,7 @@ def main() -> None:
             st.Page("app_pages/queue.py", title="Queue", icon=":material/lan:"),
             st.Page("app_pages/submit.py", title="Submit", icon=":material/upload_file:"),
             st.Page("app_pages/results.py", title="Results", icon=":material/monitoring:"),
+            st.Page("app_pages/chemiscope.py", title="Chemiscope", icon=":material/animation:"),
             st.Page("app_pages/pyscf.py", title="PySCF", icon=":material/settings:"),
             st.Page("app_pages/about.py", title="About", icon=":material/info:"),
         ],
